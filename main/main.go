aca3ed92a8c54f74a06961e3afa136bb84d4b099
antony0016 <feather881030@gmail.com>
2020-02-15 06:57:20 +0800

    init

--- main/main.go
+++ main/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"../mylib/server"
 
@@ -11,21 +13,23 @@ import (
 type api = server.RoutAndHandler
 
 // all of data that linebot needs
-var channelSecret string = "6fb3aaf4b3867e72ce0e207e36a39efd"
-var channelAccessToken string = "UB0GirpfktF+S9W04eLzMjh+1HbCzt3YxDwnbq1pVVikETNTXfWxhvAVhKPkTWjp5zws2d9wXqBfcW4NX+oZb8z8CNohTIyhSxeIpY2INu+idaD4BKG/+9vAf4djrYSoLJUrxrrA9WydULS9T0h3ywdB04t89/1O/w1cDnyilFU="
-var httpReplyURL string = "https://api.line.me/v2/bot/message/reply"
-var serverPort string = ":8000"
+var channelSecret string = ""
+var channelAccessToken string = ""
+var serverPort string = ""
 
 // linebot intance
 var bot *linebot.Client
 
 func main() {
 	var err error
+	channelSecret = os.Getenv("channelSecret")
+	channelAccessToken = os.Getenv("channelAccessToken")
+	serverPort = fmt.Sprintf(":%s", os.Getenv("PORT"))
 	apis := []api{api{Rout: "/callback", Func: HandleAllEvents}}
 
 	bot, err = linebot.New(channelSecret, channelAccessToken)
 
-	log.Println("bot:", bot, "\nerr:", err)
+	log.Println("bot:", bot, ",err:", err)
 
 	server.StartServer(serverPort, apis)
 
